916046454af2db2fef0dfa5968a79b8ea507bece
Neeraj <vishwakarmaneeraj866@gmail.com>
2020-12-16 10:25:06 +0530

    complite game

---
+++ script.js
@@ -0,0 +1,5 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'photo/sound.mp3'); 
+audio.setAttribute('autoplay', 'photo/sound.mp3'); 
+audio.loop=false;
+// audio.play();
